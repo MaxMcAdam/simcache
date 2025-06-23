c7911b51900f544861232cfe9d40288560810d2b
MaxMcAdam <mmax@fredcom.com>
2025-06-23 10:55:11 -0400

    Create a trait to represent the eviction policy

--- src/main.rs
+++ src/main.rs
@@ -4,22 +4,26 @@ use std::time::{Duration, Instant};
 
 /// accessed objects are pushed onto the back of the access_order queue
 /// therefore the oldest items are at the front
-pub struct Simcache<K, V> {
+pub struct Simcache<K, V, E> 
+where 
+    E: EvictionPolicy<K>
+{
     store: HashMap<K, (V, Option<Instant>)>,
-    access_order: VecDeque<K>,
+    eviction_policy: E,
     max_capacity: usize,
 }
 
-impl<K, V> Simcache<K, V> 
+impl<K, V, E> Simcache<K, V, E> 
 where 
     K: Eq + Hash + Clone,
     V: Clone,
+    E: EvictionPolicy<K>,
     {
         /// return a new, empty cache
         pub fn new(max_capacity: usize) -> Self {
             Simcache {
                 store: HashMap::new(),
-                access_order: VecDeque::new(),
+                eviction_policy: E::new(),
                 max_capacity,
             }
         }
@@ -28,7 +32,7 @@ where
         pub fn new_with_capacity(capacity: usize, max_capacity: usize) -> Self {
             Simcache {
                 store: HashMap::with_capacity(capacity),
-                access_order: VecDeque::new(),
+                eviction_policy: E::new(),
                 max_capacity,
             }
         }
@@ -36,29 +40,14 @@ where
         /// insert a key value pair into the cache
         /// option to include a ttl for the item
         pub fn insert(&mut self, key: K, value: V, ttl: Option<Duration>) {
-            if self.len() > self.max_capacity - 1 && self.get(&key).is_none() {
-                self.remove_oldest();
+            if self.store.len() > self.max_capacity - 1 && self.get(&key).is_none() {
+                self.eviction_policy.evict_next();
             }
             match ttl {
                 Some(x) => {self.store.insert(key.clone(), (value, Some(Instant::now() + x)));},
                 None => {self.store.insert(key.clone(), (value, None));}
             }
-            self.remove_from_access_order(&key);
-            self.access_order.push_back(key);
-        }
-
-        /// return true if the given key is present in the access_order queue already, false otherwise
-        fn remove_from_access_order(&mut self, key: &K) {
-            if let Some(pos) = self.access_order.iter().position(|k| k == key) {
-                self.access_order.remove(pos);
-            }
-        }
-
-        /// remove the key:value pair from the cache that was least recently used
-        pub fn remove_oldest(&mut self) {
-            let oldest_key = self.access_order.pop_front().unwrap();
-
-            self.remove(&oldest_key);
+            self.eviction_policy.key_used(&key);
         }
 
         /// return the value of the given key from the cache if it is not expired
@@ -91,7 +80,7 @@ where
 
         /// remove the key value pair with the given key from the cache
         pub fn remove(&mut self, key: &K) -> Option<V> {
-            self.remove_from_access_order(key);
+            self.eviction_policy.remove_key(key);
             self.store.remove(key).map(|(value, _)| value)
         }
 
@@ -101,18 +90,44 @@ where
         }
     }
 
+trait EvictionPolicy<K> {
+    fn evict_next(&mut self) -> K;
+    fn key_used(&mut self, key: &K);
+    fn remove_key(&mut self, key: &K);
+    fn new() -> Self;
+}
+
+struct LRU<K> {access_order: VecDeque<K>}
+
+impl<K: PartialEq + Clone> EvictionPolicy<K> for LRU<K> {
+    fn evict_next(&mut self) -> K {
+        return self.access_order.pop_front().expect("there should be at least one element in the eviction queue")
+    }
+    fn key_used(&mut self, key: &K) {
+        self.remove_key(key);
+        self.access_order.push_back(key.clone());
+    }
+    fn remove_key(&mut self, key: &K) {
+        if let Some(pos) = self.access_order.iter().position(|k| k == key) {
+            self.access_order.remove(pos);
+        }
+    }
+    fn new() -> Self {
+        return LRU{access_order: VecDeque::new()}
+    }
+}
+
     fn main() {
-        let mut cache = Simcache::new(2);
+        let mut cache: Simcache::<&'static str, &'static str, LRU<&'static str>> = Simcache::new(2);
 
         cache.insert("key1", "val1", None);
-        cache.insert("key2", "val2", Some(Duration::from_secs(3)));
-        cache.insert("key3", "val3", None);
+        cache.insert("key2", "val2", Some(Duration::from_secs(33)));
 
         println!("key1: {:?}", cache.get(&"key1"));
+        cache.insert("key4", "val4", None);
         println!("key2: {:?}", cache.get(&"key2"));
         println!("key3: {:?}", cache.get(&"key3"));
 
-        std::thread::sleep(Duration::from_secs(4));
-
-        println!("After expiration time - key2: {:?}", cache.get(&"key2"));
+        // std::thread::sleep(Duration::from_secs(4));
+        println!("After adding 4 - key1: {:?}", cache.get(&"key1"));
     }
